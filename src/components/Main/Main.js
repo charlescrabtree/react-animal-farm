cd72aa812eeb04179703cea58932078ceec348d6
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-09-12 15:08:35 -0700

    staged changes that did not commit the first time

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './Main.css';
+import backgroundImage from '../../background.png';
+import { animals } from '../../data.js';
+import Animal from '../Animal/Animal.js';
 
 export default function Main() {
-  return <main></main>;
+  return <main style={{ backgroundImage: `url(${backgroundImage})` }}>
+    {animals.map((item) => (
+      <Animal key={item.name} {...item} />
+    ))}
+  </main>;
 }
